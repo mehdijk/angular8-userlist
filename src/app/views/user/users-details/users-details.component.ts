48a0ddd247b5fedf08efe9459d2fdc1be23a64bd
mehdi <you@example.com>
2019-09-10 17:01:07 +0200

    refactoring the code by rxjs operators

--- src/app/views/user/users-details/users-details.component.ts
+++ src/app/views/user/users-details/users-details.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../user.service';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { debounceTime, concatMap, map } from 'rxjs/operators';
+import { debounceTime, concatMap, map, switchMap, tap, filter, mergeMap, toArray } from 'rxjs/operators';
 import { User } from '../user';
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { Dog } from '../dog';
 
 
@@ -14,12 +14,6 @@ import { Dog } from '../dog';
   styleUrls: ['./users-details.component.scss']
 })
 export class UsersDetailsComponent implements OnInit {
-
-  constructor(readonly route: ActivatedRoute,
-              readonly service: UserService,
-              readonly fb: FormBuilder,
-              readonly router: Router) { }
-
   id: string;
   dogs: Dog[] = new Array<Dog>();
   allDogs: Dog[];
@@ -48,57 +42,87 @@ export class UsersDetailsComponent implements OnInit {
     return this.userForm.get('age');
   }
 
+  constructor(readonly route: ActivatedRoute,
+    readonly service: UserService,
+    readonly fb: FormBuilder,
+    readonly router: Router) { }
+
   ngOnInit() {
-    this.id = this.route.snapshot.params.id;
-    if (this.id) {
-      this.loadDetail();
-      this.userForm.valueChanges.pipe(
-        debounceTime(500),
-        concatMap(user => this.updateUser({...user, id: +this.id, favoriteDogs: this.dogs.map((x) => x.id)}))
-      ).subscribe(user => {
-        this.userForm.patchValue(user, {emitEvent: false});
-      });
-    }
+    // this.id = this.route.snapshot.params.id;
+    this.route.paramMap.pipe(
+      map(params => {
+        this.id = params.get('id');
+        return this.id;
+      }),
+      filter(id => Boolean(id)),
+      switchMap(() => {
+        return this.loadDetail().pipe(
+          concatMap(details => {
+            this.userForm.patchValue(details);
+            return this.userForm.valueChanges.pipe(
+              debounceTime(500),
+              concatMap(user => this.updateUser({ ...user, id: +this.id, favoriteDogs: this.dogs.map((x) => x.id) }))
+            );
+          })
+        );
+      }),
+    ).subscribe((user) => {
+      this.userForm.patchValue(user, { emitEvent: false });
+    });
+
+
     this.service.getAllDogs().subscribe(dogs => this.allDogs = dogs);
   }
 
-  loadDetail() {
-    this.service.getUserById(this.id).subscribe(user => {
-      this.userForm.patchValue(user);
-      if (user.favoriteDogs) {
-        user.favoriteDogs.forEach((id) => this.service.getDogById(id)
-        .subscribe(val => this.dogs.push(val)));
-      }
-    });
+  loadDetail(): Observable<User> {
+    return this.service.getUserById(this.id).pipe(
+      tap(user => {
+        this.userForm.patchValue(user);
+      }),
+      concatMap(user => {
+        return of(user).pipe(
+          mergeMap(user => user.favoriteDogs),
+          mergeMap(dogId => {
+            return this.service.getDogById(dogId);
+          }),
+          toArray(),
+          map(favoriteDogs => {
+            this.dogs = favoriteDogs;
+            return user;
+          }),
+        );
+      }),
+    );
   }
 
   addNew() {
-    if (this.userForm.status === 'INVALID') {
+    if (this.userForm.invalid) {
       this.userForm.markAllAsTouched();
-      return ;
+      return;
     }
-    const user: User = {...this.userForm.value, favoriteDogs: this.dogs.map(dog => dog.id)};
+
+    const user: User = { ...this.userForm.value, favoriteDogs: this.dogs.map(dog => dog.id) };
     this.service.addNew(user).subscribe(res => this.router.navigate(['users']));
   }
 
   updateUser(user: User): Observable<User> {
     if (this.userForm.status === 'INVALID') {
       this.userForm.markAllAsTouched();
-      return of(user) ;
+      return of(user);
     } else {
-    return this.service.updateUser(user);
-  }
+      return this.service.updateUser(user);
+    }
   }
 
   getDogById(id: number): Observable<Dog> {
     return this.service.getDogById(id);
   }
 
-  delete(dog: Dog){
+  delete(dog: Dog) {
     const index = this.dogs.indexOf(dog, 0);
     if (index > -1) {
-    this.dogs.splice(index, 1);
-    this.userForm.updateValueAndValidity();
+      this.dogs.splice(index, 1);
+      this.userForm.updateValueAndValidity();
     }
   }
 
